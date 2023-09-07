f5e020d71bcb4bf1b6d23b1dc8fa2c4b318a9eae
kimtheanh <kimtheanh1208@gmail.com>
2023-09-07 16:10:06 +0700

    a

--- apps/files/js/filelist.js
+++ apps/files/js/filelist.js
@@ -1690,7 +1690,7 @@
 
 			// filename td
 			td = $('<td class="filename"></td>');
-			console.log('aaaaa')
+			console.log('aaaaaaaaaaa')
 
 			var spec = this.fileActions.getDefaultFileAction(mime, type, permissions);
 			// linkUrl
